573c76e0e20eedd84922cb5eea6f1e6edcad9923
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-11-13 00:40:21 +0100

    fix: working copy/paste of defaultFiles

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
 
 // Require Node.js Dependencies
-const { readdir, readFile, writeFile } = require("fs").promises;
+const {
+    createReadStream, createWriteStream,
+    promises: { readdir, readFile, writeFile }
+} = require("fs");
 const { join } = require("path");
 
 // Require Third-party Dependencies
 const execa = require("execa");
 const inquirer = require("inquirer");
 
+// Require Internal Dependencies
+const DEFAULT_PKG = require("./template/package.json");
+
 // CONSTANTS
 const ROOT_DIR = join(__dirname, "..");
 const TEMPLATE_DIR = join(ROOT_DIR, "template");
+const DEFAULT_FILES_DIR = join(TEMPLATE_DIR, "defaultFiles");
+
+// QUESTIONS
+const GEN_QUESTIONS = [
+    {
+        message: "What is the name of your project",
+        type: "input",
+        name: "projectname"
+    },
+    {
+        message: "Add a description (optional)",
+        type: "input",
+        name: "projectdesc"
+    }
+];
 
 /**
  * @async
@@ -28,24 +49,20 @@ async function main() {
     await execa("npm init -y");
 
     // Write default projects files
-    (await readdir(
-        join(TEMPLATE_DIR, "defaultFiles")
-    )).map((file) => writeFile(file));
+    const tplFiles = await readdir(DEFAULT_FILES_DIR);
+    for (const fileName of tplFiles) {
+        const rS = createReadStream(join(DEFAULT_FILES_DIR, fileName), {
+            highWaterMark: 1024
+        });
+        const wS = createWriteStream(join(cwd, fileName));
+        for await (const buf of rS) {
+            wS.write(buf);
+        }
+        wS.end();
+    }
 
     // Ask projectName and projectDesc
-    const questions = [
-        {
-            message: "What is the name of your project",
-            type: "input",
-            name: "projectname"
-        },
-        {
-            message: "Add a description (optional)",
-            type: "input",
-            name: "projectdesc"
-        }
-    ];
-    const response = await inquirer.prompt(questions);
+    const response = await inquirer.prompt(GEN_QUESTIONS);
 
     // Handle Package.json
     {
@@ -55,8 +72,9 @@ async function main() {
         const obj = JSON.parse(buf.toString());
         obj.name = `@slimio/${response.projectname}`;
         obj.description = response.projectdesc;
+        const finalPayload = Object.assign(obj, Object.create(DEFAULT_PKG));
 
-        await writeFile(cwdPackage, JSON.stringify(obj, null, 2));
+        await writeFile(cwdPackage, JSON.stringify(finalPayload, null, 2));
     }
 
     // Handle README.md
