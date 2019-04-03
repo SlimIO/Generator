78ad03aefc240a243fdd1fa5d9df1470da950c74
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-04-03 05:31:57 +0200

    refactor: improve transfertFiles method

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,10 @@
 // Require Node.js Dependencies
-const {
-    createReadStream, createWriteStream,
-    promises: { readdir }
-} = require("fs");
+const { createReadStream, createWriteStream, promises: { readdir } } = require("fs");
+const { promisify } = require("util");
 const { join } = require("path");
+const stream = require("stream");
+
+const pipeline = promisify(stream.pipeline);
 
 /**
  * @async
@@ -15,15 +16,12 @@ const { join } = require("path");
  */
 async function transfertFiles(currDir, targetDir) {
     const AllFiles = await readdir(currDir);
+
     for (const fileName of AllFiles) {
-        const rS = createReadStream(join(currDir, fileName), {
-            highWaterMark: 1024
-        });
-        const wS = createWriteStream(join(targetDir, fileName));
-        for await (const buf of rS) {
-            wS.write(buf);
-        }
-        wS.end();
+        await pipeline(
+            createReadStream(join(currDir, fileName)),
+            createWriteStream(join(targetDir, fileName))
+        );
     }
 }
 
